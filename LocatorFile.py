7a578487f1dcfe674a3632fe01554beeb16c2390
walter <1654226880301@ingenieria.usac.edu.gt>
2022-10-28 17:48:10 -0600

    Multidiferenciador Adaptado

--- LocatorFile.py
+++ LocatorFile.py
@@ -1,5 +1,7 @@
 import cv2
 import pyautogui
+import numpy as np
+import imutils
 
 
 #x, y = pyautogui.locateCenterOnScreen('calc7key.png')
@@ -21,8 +23,63 @@ class Locator:
             print("Image not found")
         return self.x, self.y, dimensions[1], dimensions[0]
     
-    def locate_by_template(self,):
-        pass
+    
+
+    def multi_scale_locator(self, template_url,enviroment_url):
+
+        template = cv2.imread(template_url)
+        template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+        template = cv2.Canny(template, 50, 200)
+        template_height, template_width = template.shape[:2]
+        #cv2.imshow('Template', template)
+        #cv2.waitKey(0)
+
+        enviroment = cv2.imread(enviroment_url)
+        enviroment = cv2.cvtColor(enviroment, cv2.COLOR_BGR2GRAY)
+        #enviroment = cv2.Canny(enviroment, 50, 200)
+        enviroment_height, enviromente_width = enviroment.shape[:2]
+        #cv2.imshow('Enviroment', enviroment)
+        #cv2.waitKey(0)
+
+        rectangles = []
+        threshold = 0.13
+        
+        # Iteracion  sobre 20 escalas diferentes entre 0.2 y 1
+        for scale in np.linspace(0.2, 1.0, 20)[::-1]:
+            # Redimension de la imagen con base a la escala actual.
+            resized = imutils.resize(template, width=int(template.shape[1] * scale))
+            w, h = resized.shape[:2]
+
+            #result = cv2.matchTemplate(enviroment,resized, cv2.TM_CCOEFF_NORMED)
+            
+            result = cv2.matchTemplate(enviroment,resized, cv2.TM_CCOEFF)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+            #yloc, xloc = np.where(result >= threshold)
+            
+            
+            #for (x, y) in zip(xloc, yloc):
+                #rectangles.append([int(x), int(y), int(w), int(h)])
+                #rectangles.append([int(x), int(y), int(w), int(h)])
+            
+            rectangles.append([int(max_loc[0]), int(max_loc[1]), int(w), int(h)])
+            rectangles.append([int(max_loc[0]), int(max_loc[1]), int(w), int(h)])
+            
+            #print("Rectangles: ", len(rectangles))
+        
+        rectangles, weights = cv2.groupRectangles(rectangles, 1, 0.2)
+        for (x,y,w,h) in rectangles:
+            cv2.rectangle(enviroment, (x,y), (x+w, y+h), (0,255,255), 2)
+        
+        cv2.imshow('Imagen', enviroment)
+        cv2.waitKey(0)
+        
+        
+        
+
+
+
+
 
     
 
--- main.py
+++ main.py
@@ -5,11 +5,46 @@ from LocatorFile import Locator
 def main():
 
     Lc = Locator()
-    x,y,width,height = Lc.locate('./Img/specific/gmail_icon.png')
+    Lc.multi_scale_locator('./Img/templates/chrome.png','./Img/enviroments/sample.png')
 
-    if(x != -1 and y != -1):
-        D = Draw()
-        D.draw_frame_by_center(x,y,width,height,D.green)
+    
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#x,y,width,height = Lc.locate('./Img/specific/gmail_icon.png')
+#if(x != -1 and y != -1):
+#        D = Draw()
+#        D.draw_frame_by_center(x,y,width,height,D.green)
